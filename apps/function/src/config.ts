c8d3f6102d3c2c554db304b77d927a64c313d811
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-11-21 11:09:43 +0000

    feat: add horizon api client to service

--- apps/function/src/config.ts
+++ apps/function/src/config.ts
@@ -1,19 +1,32 @@
 import { loadEnvFile } from 'node:process';
 
 export interface Config {
-	aListBuilder: {
-		schedule: string;
-	};
-	bTransformer: {
-		schedule: string;
-	};
-	cDocumentHandler: {
-		schedule: string;
-	};
-	dValidator: {
-		schedule: string;
-	};
 	database: string;
+	functions: {
+		aListBuilder: {
+			schedule: string;
+		};
+		bTransformer: {
+			schedule: string;
+		};
+		cDocumentHandler: {
+			schedule: string;
+		};
+		dValidator: {
+			schedule: string;
+		};
+	};
+	horizon: {
+		apiEndpoint: string;
+		apiTimeoutMs?: number;
+	};
+	manageAppeals: {
+		apiEndpoint: string;
+		documents: {
+			accountName: string;
+			containerName: string;
+		};
+	};
 }
 
 export function loadConfig(): Config {
@@ -23,30 +36,60 @@ export function loadConfig(): Config {
 
 	// get values from the environment
 	const {
-		LIST_BUILDER_SCHEDULE,
-		TRANSFORMER_SCHEDULE,
-		DOCUMENT_HANDLER_SCHEDULE,
-		VALIDATOR_SCHEDULE,
+		FUNC_LIST_BUILDER_SCHEDULE,
+		FUNC_TRANSFORMER_SCHEDULE,
+		FUNC_DOCUMENT_HANDLER_SCHEDULE,
+		FUNC_VALIDATOR_SCHEDULE,
+		HORIZON_API_ENDPOINT,
+		HORIZON_API_TIMEOUT_MS,
+		MANAGE_APPEALS_API_ENDPOINT,
+		MANAGE_APPEALS_DOCUMENTS_ACCOUNT_NAME,
+		MANAGE_APPEALS_DOCUMENTS_CONTAINER_NAME,
 		SQL_CONNECTION_STRING
 	} = process.env;
 
+	if (!HORIZON_API_ENDPOINT) {
+		throw new Error('MANAGE_APPEALS_API_ENDPOINT is required');
+	}
+	if (!MANAGE_APPEALS_API_ENDPOINT) {
+		throw new Error('MANAGE_APPEALS_API_ENDPOINT is required');
+	}
+	if (!MANAGE_APPEALS_DOCUMENTS_ACCOUNT_NAME) {
+		throw new Error('MANAGE_APPEALS_DOCUMENTS_ACCOUNT_NAME is required');
+	}
+	if (!MANAGE_APPEALS_DOCUMENTS_CONTAINER_NAME) {
+		throw new Error('MANAGE_APPEALS_DOCUMENTS_CONTAINER_NAME is required');
+	}
 	if (!SQL_CONNECTION_STRING) {
 		throw new Error('SQL_CONNECTION_STRING is required');
 	}
 
 	return {
-		aListBuilder: {
-			schedule: LIST_BUILDER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+		database: SQL_CONNECTION_STRING,
+		functions: {
+			aListBuilder: {
+				schedule: FUNC_LIST_BUILDER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+			},
+			bTransformer: {
+				schedule: FUNC_TRANSFORMER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+			},
+			cDocumentHandler: {
+				schedule: FUNC_DOCUMENT_HANDLER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+			},
+			dValidator: {
+				schedule: FUNC_VALIDATOR_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+			}
 		},
-		bTransformer: {
-			schedule: TRANSFORMER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
+		horizon: {
+			apiEndpoint: HORIZON_API_ENDPOINT,
+			apiTimeoutMs: HORIZON_API_TIMEOUT_MS ? parseInt(HORIZON_API_TIMEOUT_MS) : undefined
 		},
-		cDocumentHandler: {
-			schedule: DOCUMENT_HANDLER_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
-		},
-		dValidator: {
-			schedule: VALIDATOR_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
-		},
-		database: SQL_CONNECTION_STRING
+		manageAppeals: {
+			apiEndpoint: MANAGE_APPEALS_API_ENDPOINT,
+			documents: {
+				accountName: MANAGE_APPEALS_DOCUMENTS_ACCOUNT_NAME,
+				containerName: MANAGE_APPEALS_DOCUMENTS_CONTAINER_NAME
+			}
+		}
 	};
 }
--- apps/function/src/service.ts
+++ apps/function/src/service.ts
@@ -1,6 +1,7 @@
 import { newDatabaseClient } from '@pins/appeals-migration-database';
 import type { PrismaClient } from '@pins/appeals-migration-database/src/client/client.ts';
 import type { Config } from './config.ts';
+import { HorizonApiClient } from '@pins/appeals-migration-lib/horizon/horizon-api-client.ts';
 
 /**
  * This class encapsulates all the services and clients for the application
@@ -8,6 +9,7 @@ import type { Config } from './config.ts';
 export class FunctionService {
 	#config: Config;
 	dbClient: PrismaClient;
+	horizonApiClient: HorizonApiClient;
 
 	constructor(config: Config) {
 		this.#config = config;
@@ -15,21 +17,22 @@ export class FunctionService {
 			throw new Error('database config is required');
 		}
 		this.dbClient = newDatabaseClient(config.database);
+		this.horizonApiClient = new HorizonApiClient(config.horizon.apiEndpoint, config.horizon.apiTimeoutMs);
 	}
 
 	get aListBuilderSchedule() {
-		return this.#config.aListBuilder.schedule;
+		return this.#config.functions.aListBuilder.schedule;
 	}
 
 	get bTransformerSchedule() {
-		return this.#config.bTransformer.schedule;
+		return this.#config.functions.bTransformer.schedule;
 	}
 
 	get cDocumentHandlerSchedule() {
-		return this.#config.cDocumentHandler.schedule;
+		return this.#config.functions.cDocumentHandler.schedule;
 	}
 
 	get dValidatorSchedule() {
-		return this.#config.dValidator.schedule;
+		return this.#config.functions.dValidator.schedule;
 	}
 }
