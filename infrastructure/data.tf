f75bd47ce021db196f2d70cab8df3184ad81e1f9
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-12-01 15:32:14 +0000

    feat(infrastructure): network and key vault

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,8 +1,29 @@
-# data "azurerm_client_config" "current" {}
+data "azurerm_client_config" "current" {}
 #
-# data "azurerm_virtual_network" "tooling" {
-#   name                = var.tooling_config.network_name
-#   resource_group_name = var.tooling_config.network_rg
-#
-#   provider = azurerm.tooling
-# }
+data "azurerm_virtual_network" "tooling" {
+  name                = var.tooling_config.network_name
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
+data "azurerm_private_dns_zone" "database" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
+data "azurerm_private_dns_zone" "app_service" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
+data "azurerm_private_dns_zone" "key_vault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
--- infrastructure/environments/test.tfvars
+++ infrastructure/environments/test.tfvars
@@ -1,1 +1,7 @@
 environment = "test"
+
+vnet_config = {
+  address_space             = "10.35.4.0/22"
+  apps_subnet_address_space = "10.35.4.0/24"
+  main_subnet_address_space = "10.35.5.0/24"
+}
