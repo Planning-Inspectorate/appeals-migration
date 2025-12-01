a30843cf4a9dbcfb675c68f28fc332736843f98b
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-12-01 13:35:18 +0000

    chore(infrastructure): remove unused variables

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,11 +1,9 @@
 locals {
-  org                = "pins"
-  service_name       = "appeals-migration"
-  primary_location   = "uk-south"
-  secondary_location = "uk-west"
+  org              = "pins"
+  service_name     = "appeals-migration"
+  primary_location = "uk-south"
 
-  resource_suffix           = "${local.service_name}-${var.environment}"
-  secondary_resource_suffix = "${local.service_name}-secondary-${var.environment}"
+  resource_suffix = "${local.service_name}-${var.environment}"
 
   tags = {
     CreatedBy   = "terraform"
