963ea08449c970e02fe4fbe1365b24cf4470ffb3
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-12-01 15:54:21 +0000

    feat(infrastructure): app insights and log analytics

--- infrastructure/monitoring.tf
+++ infrastructure/monitoring.tf
@@ -1,3 +1,25 @@
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "${local.org}-log-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  daily_quota_gb      = var.monitoring_config.log_daily_cap
+
+  tags = local.tags
+}
+
+resource "azurerm_application_insights" "main" {
+  name                 = "${local.org}-ai-${local.resource_suffix}"
+  location             = module.primary_region.location
+  resource_group_name  = azurerm_resource_group.primary.name
+  workspace_id         = azurerm_log_analytics_workspace.main.id
+  application_type     = "web"
+  daily_data_cap_in_gb = 0.1
+
+  tags = local.tags
+}
+
 resource "azurerm_monitor_action_group" "appeals_migration_tech" {
   name                = "pins-ag-appeals-migration-tech-${var.environment}"
   resource_group_name = azurerm_resource_group.primary.name
