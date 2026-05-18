c6b104773f3c128076ca8fa67db0cea37085ef92
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-05-18 09:13:39 +0100

    feat(infrastructure): function storage have private endpoints on

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -48,3 +48,10 @@ data "azurerm_private_dns_zone" "synapse_sql" {
 
   provider = azurerm.tooling
 }
+
+data "azurerm_private_dns_zone" "storage" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
